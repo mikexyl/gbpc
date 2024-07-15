c0835722834265450b524b54563284808078299d
Mike Xiangyu Liu <liu.xiangyu@ucy.ac.cy>
2024-07-15 18:00:03 +0300

    WIP, removing belief merging for debugging

--- include/gbpc/variable.h
+++ include/gbpc/variable.h
@@ -19,18 +19,23 @@ public:
       : belief_(initial), mu_(initial.mu_), Sigma_(initial.Sigma_) {}
 
   void update(const std::vector<Gaussian<Dim>> &messages) {
-    auto eta = belief_.eta_;
-    auto lambda = belief_.lambda_;
+    // auto eta = belief_.eta_;
+    // auto lambda = belief_.lambda_;
+
+    // for (const auto &message : messages) {
+    //   lambda += message.lambda_;
+    //   eta += message.eta_;
+    // }
+
+    // belief_.eta_ = eta;
+    // belief_.lambda_ = lambda;
+    // Sigma_ = lambda.inverse();
+    // mu_ = Sigma_ * belief_.eta_;
 
     for (const auto &message : messages) {
-      lambda += message.lambda_;
-      eta += message.eta_;
+      mu_ = message.mu_;
+      Sigma_ = message.Sigma_;
     }
-
-    belief_.eta_ = eta;
-    belief_.lambda_ = lambda;
-    Sigma_ = lambda.inverse();
-    mu_ = Sigma_ * belief_.eta_;
   }
 
   auto mu() const { return mu_; }
